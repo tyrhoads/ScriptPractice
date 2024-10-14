d2f35931381478f6a665ea8992b1fa1aa8febd4f
Tyler Rhoads <tyrhoads@gmail.com>
2024-10-13 22:34:14 -0400

    got button to work

--- palindromeScript.js
+++ palindromeScript.js
@@ -1,3 +1,18 @@
-const userInput = document.getElementById('text-input')
+const userInput = document.getElementById('text-input');
+const checkButton = document.getElementById('check-btn');
 
 
+
+
+function getText(){
+    const inputContainer = userInput.value;
+    console.log(inputContainer);
+    userInput.value="";
+
+
+
+}
+
+getText();
+
+checkButton.addEventListener("click", getText);

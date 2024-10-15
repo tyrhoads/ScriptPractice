cf6b4804366ff3577ac0ac2c0133f5ffaa5236cc
Tyler Rhoads <tyrhoads@gmail.com>
2024-10-14 23:37:11 -0400

    slowly

--- palindromeScript.js
+++ palindromeScript.js
@@ -1,18 +1,31 @@
 const userInput = document.getElementById('text-input');
 const checkButton = document.getElementById('check-btn');
+const result = document.getElementById('result');
 
 
 
-
-function getText(){
-    const inputContainer = userInput.value;
+function getText(input)
+{
+    const inputContainer = input.value;
     console.log(inputContainer);
     userInput.value="";
-
-
-
+    result.replaceChildren();
+    
 }
 
-getText();
 
-checkButton.addEventListener("click", getText);
+
+getText(userInput.value);
+
+
+checkButton.addEventListener("click", () =>{
+    getText(userInput); 
+});
+
+// Execute a function when the user presses a key on the keyboard in the input field
+userInput.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key
+  if (event.key === "Enter") {
+    getText(userInput); 
+  }
+});

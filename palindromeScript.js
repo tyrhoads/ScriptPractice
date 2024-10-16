17c6fa02b68113a7667c82742e9deb14bb3541c6
Tyler Rhoads <tyrhoads@gmail.com>
2024-10-15 23:14:16 -0400

    finished

--- palindromeScript.js
+++ palindromeScript.js
@@ -6,26 +6,43 @@ const result = document.getElementById('result');
 
 function getText(input)
 {
-    const inputContainer = input.value;
-    console.log(inputContainer);
-    userInput.value="";
-    result.replaceChildren();
-    
+   
+    const inputContainer = input;
+    if (input === '') {
+        alert('Please input a value');
+        return;
+      }
+    // Remove the previous result
+  result.replaceChildren();
+
+  const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
+  let resultMsg = `<strong>${inputContainer}</strong> ${
+    lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'
+  } a palindrome.`;
+
+  const pTag = document.createElement('p');
+  pTag.className = 'text-input';
+  pTag.innerHTML = resultMsg;
+  result.appendChild(pTag);
+
+  // Show the result.
+  result.classList.remove('hidden');
 }
 
 
 
-getText(userInput.value);
 
 
 checkButton.addEventListener("click", () =>{
-    getText(userInput); 
+    getText(userInput.value); 
+    userInput.value="";
 });
 
 // Execute a function when the user presses a key on the keyboard in the input field
 userInput.addEventListener("keypress", function(event) {
   // If the user presses the "Enter" key
   if (event.key === "Enter") {
-    getText(userInput); 
+    getText(userInput.value);
+    userInput.value="";
   }
 });

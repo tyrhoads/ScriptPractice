2c37966f30037f883c35825ded962e894a8e281b
Tyler Rhoads <tyrhoads@gmail.com>
2024-10-22 23:08:47 -0400

    suffled array

--- santaScript.js
+++ santaScript.js
@@ -2,8 +2,8 @@ const userInput = document.getElementById('text-input');
 const checkButton = document.getElementById('check-btn');
 const result = document.getElementById('result');
 const people =[];
-
-
+const peopleShuffled = [];
+const sButton = document.getElementById('sButton');
 function getText(input)
 {
    
@@ -14,27 +14,56 @@ function getText(input)
       }
     // Remove the previous result
   //result.replaceChildren();
-   people.push(inputContainer);
-   console.log(people);   
-  const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
-  let resultMsg = `${inputContainer}`;
-
-  const pTag = document.createElement('p');
+     
+  const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toUpperCase();
+  let resultMsg = `${lowerCaseStr}`;
+  people.push(lowerCaseStr); 
+  peopleShuffled.push(lowerCaseStr);
+  const pTag = document.createElement('h3');
   pTag.className = 'text-input';
   pTag.innerHTML = resultMsg;
   result.appendChild(pTag);
+  
+
+  
+
 
   // Show the result.
   result.classList.remove('hidden');
-}
+  sButton.classList.remove('hidden');
+};
 
+function shuffle(array)
+{
+    let currentIndex = array.length;
+      
+    // While there remain elements to shuffle...
+     while (currentIndex != 0) {
+      
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+      
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+    array[randomIndex], array[currentIndex]];
+    }
+};
+
+function santa (array, shuffledArray){
+    shuffle(shuffledArray);
+ console.log(array);
+ console.log(shuffledArray);
+}
 
 
 
 
 checkButton.addEventListener("click", () =>{
     getText(userInput.value); 
+    santa(people,peopleShuffled );
     userInput.value="";
+    
 });
 
 // Execute a function when the user presses a key on the keyboard in the input field
@@ -42,6 +71,7 @@ userInput.addEventListener("keypress", function(event) {
   // If the user presses the "Enter" key
   if (event.key === "Enter") {
     getText(userInput.value);
+    santa(people,peopleShuffled );
     userInput.value="";
   }
 });

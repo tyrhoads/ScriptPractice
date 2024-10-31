6eb65cb1e8f88cd309aebbaed5e83ad7009bf45b
Tyler Rhoads <tyrhoads@gmail.com>
2024-10-30 23:03:08 -0400

    finished for today

--- todoScript.js
+++ todoScript.js
@@ -12,3 +12,11 @@ const descriptionInput = document.getElementById("description-input");
 
 const taskData = [];
 let currentTask = {};
+
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
+
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal();
+});

ba43d0d998925ce7b38ae72b920fa8188b70a69f
Tyler Rhoads <tyrhoads@gmail.com>
2024-11-06 22:52:13 -0500

    ad

--- todoScript.js
+++ todoScript.js
@@ -35,7 +35,24 @@ taskForm.addEventListener("submit", (e) => {
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
     id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-    
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
   };
-  console.log(taskObj);
+
+   if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+
+  taskData.forEach(({id, title, date, description}) => {
+      tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          
+        </div>
+      `
+    }
+  );
 });

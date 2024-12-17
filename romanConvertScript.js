f735ceaaa8c240d98fb9f48f4007333b446a04e3
Tyler Rhoads <tyrhoads@gmail.com>
2024-12-16 23:21:19 -0500

    unhide when clicked and number pops up

--- romanConvertScript.js
+++ romanConvertScript.js
@@ -1,7 +1,38 @@
-const number = document.getElementById('number')
+
 const convertButton = document.getElementById('convert-btn');
 const output = document.getElementById('output');
 
+const convertToRoman = num => {
+    const ref = [
+      ['M', 1000],
+      ['CM', 900],
+      ['D', 500],
+      ['CD', 400],
+      ['C', 100],
+      ['XC', 90],
+      ['L', 50],
+      ['XL', 40],
+      ['X', 10],
+      ['IX', 9],
+      ['V', 5],
+      ['IV', 4],
+      ['I', 1]
+    ];
+    const res = [];
+  
+    ref.forEach(function (arr) {
+      while (num >= arr[1]) {
+        res.push(arr[0]);
+        num -= arr[1];
+      }
+    });
+
+}
+
 convertButton.addEventListener("click", () =>{
-    console.log(number.value)
+    const number = document.getElementById('number').value;
+    const int = parseInt(number,10)
+    output.innerText = int;
+    output.removeAttribute("hidden")
+    
 });

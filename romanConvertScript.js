47f9cf45403847598a762a7e4ac6c38f5d1a7244
Tyler Rhoads <tyrhoads@gmail.com>
2024-12-16 22:33:25 -0500

    added stuff

---
+++ romanConvertScript.js
@@ -0,0 +1,7 @@
+const number = document.getElementById('number')
+const convertButton = document.getElementById('convert-btn');
+const output = document.getElementById('output');
+
+convertButton.addEventListener("click", () =>{
+    console.log(number.value)
+});

9e06f4c5e8abb9b72a56d90da55fe02f43bb5283
Tyler Rhoads <tyrhoads@gmail.com>
2025-01-09 23:02:30 -0500

    aded git add sp
    exit
    ;

--- spamScript.js
+++ spamScript.js
@@ -4,9 +4,9 @@ const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+\s*(?:hundred|thousand|million|billion)?\s+dollars/i;
+const freeRegex = /fr[e3][e3] mon[e3]y/i;
 
-
-const denyList = [helpRegex, dollarRegex];
+const denyList = [helpRegex, dollarRegex, freeRegex];
 
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
